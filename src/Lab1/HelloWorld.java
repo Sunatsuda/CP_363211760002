6784a72843c983bc3a1838cc8d5eb8741a7c9b3e
Natsuda <natsuda.t@rmutsvmail.com>
2020-07-14 17:54:49 +0700

    add code 4 lines

--- src/Lab1/HelloWorld.java
+++ src/Lab1/HelloWorld.java
@@ -5,6 +5,11 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World !!!");
         System.out.println("Natsuda Thongsuk");
+        System.out.println("SU");
+        System.out.println("0918325953");
+        System.out.println("21");
+        System.out.println("152");
+
 
     }
 
