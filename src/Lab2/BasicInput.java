d039b06d0fcd2fd7fc8a740e2c2ba72c1005f071
Natsuda <natsuda.t@rmutsvmail.com>
2020-08-04 19:04:01 +0700

    Lab 2 : basic input

---
+++ src/Lab2/BasicInput.java
@@ -0,0 +1,44 @@
+package Lab2;
+
+import java.util.Scanner;
+
+public class BasicInput {
+
+        public  static  void  main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+
+            String name;
+            System.out.println("What is your name? : ");
+            name = scanner.nextLine();
+            System.out.println("Your name is " + name);
+
+            System.out.println("What is your major? : ");
+            String major = scanner.nextLine();
+            System.out.println("Your major is "+major);
+
+            System.out.println("What is your age? : ");
+            String age = scanner.nextLine();
+            System.out.println("Your age is "+age);
+
+            System.out.println("What is your email? : ");
+            String email = scanner.nextLine();
+            System.out.println("Your email is "+email);
+
+            //age
+            //email
+        }//main
+
+
+
+
+    }//main
+
+
+
+
+
+
+
+
+
