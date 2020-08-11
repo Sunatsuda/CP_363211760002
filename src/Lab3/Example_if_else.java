ce56104ca54e6501d1777b38dd6d77aac6626cfe
Natsuda <natsuda.t@rmutsvmail.com>
2020-08-11 18:42:52 +0700

    Lab 2 : basic input

---
+++ src/Lab3/Example_if_else.java
@@ -0,0 +1,27 @@
+package Lab3;
+
+import java.util.Scanner;
+
+public class Example_if_else {
+
+    public  static void  main(String[] args) {
+
+        Scanner SC = new Scanner(System.in);
+
+        System.out.println("Enter integer 1: ");
+        int X = SC.nextInt();
+        System.out.println("Enter integer 2: ");
+        int Y = SC.nextInt();
+
+        //condition
+        if(X>Y) {
+           System.out.println(X + " grater than " +Y);
+           System.out.println("X grater than Y.");
+        } else
+            System.out.println(X+" less than "+Y);
+
+        System.out.println("Good Bye.");
+
+
+    }//main
+}//main
