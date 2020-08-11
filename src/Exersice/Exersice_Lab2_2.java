9c54097059dfbc16965cea0e4dac09e39341f6ae
Natsuda <natsuda.t@rmutsvmail.com>
2020-08-11 16:41:04 +0700

    Lab 2 : basic input

---
+++ src/Exersice/Exersice_Lab2_2.java
@@ -0,0 +1,20 @@
+package Exersice;
+
+import java.util.Scanner;
+
+public class Exersice_Lab2_2 {
+
+      public  static void  main(String[] args) {
+          Scanner scanner = new Scanner(System.in);
+          System.out.print("ระบุความยาวของฐานสามเหลี่ยม: ");
+          double base = scanner.nextDouble();
+          System.out.print("ระบุความสูงของสามเหลี่ยม: ");
+          double height = scanner.nextDouble();
+
+          double area = 0.5 * base * height;
+          System.out.println("พื้นที่สามเหลี่ยม คือ " +area);
+
+
+      }
+    }
+
--- src/Lab2/Exercise_Lab2_1.java
+++
@@ -1,6 +0,0 @@
-package Lab2;
-
-public class Exercise_Lab2_1 {
-    public static void main(String[] args) {
-
-
